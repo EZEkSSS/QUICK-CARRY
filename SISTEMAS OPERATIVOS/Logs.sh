f57a180a0715e0a7cd7a3aa67427a89ab8ac57e9
MatiasMoser <120841976+MatiasMoser@users.noreply.github.com>
2023-11-15 21:31:48 -0300

    Update Logs.sh

--- SISTEMAS OPERATIVOS/Logs.sh
+++ SISTEMAS OPERATIVOS/Logs.sh
@@ -3,30 +3,29 @@ opcion=0
 echo "Bienvenido al registro logs"
 while [ "$opcion" -ne 4 ]
 do
-echo "Estas son sus opciones"
-echo "1.Ver Logins"
-echo "2.Ver actividades"
-echo "3.Ver logins y actividades de un usuario específico"
-echo "4.volver"
-read opcion
-if [ "$opcion" -eq 1 ]; then
-clear
-last -F
-elif [ "$opcion" -eq 2 ]; then
-clear
-w what
-elif [ "$opcion" -eq 3 ]; then
-clear
-echo "Escriba el usuario del cual desea saber"
-read user
-if id "$user" &>/dev/null; then
-echo "Información sobre el usuario $user"
-w what "$user"
-else
-  echo "El usuario $user no existe"
-elif [ "$opcion" -eq 4 ]: then
-sh Computos.sh
- 
-fi
-fi
+  echo "Estas son sus opciones"
+  echo "1.Ver Logins"
+  echo "2.Ver actividades"
+  echo "3.Ver logins y actividades de un usuario específico"
+  echo "4.volver"
+  read opcion
+  if [ "$opcion" -eq 1 ]; then
+    clear
+    last -F
+  elif [ "$opcion" -eq 2 ]; then
+    clear
+    w what
+  elif [ "$opcion" -eq 3 ]; then
+    clear
+    echo "Escriba el usuario del cual desea saber"
+    read user
+    if id "$user" &>/dev/null; then
+      echo "Información sobre el usuario $user"
+      w what "$user"
+    else
+      echo "El usuario $user no existe"
+    elif [ "$opcion" -eq 4 ]: then
+        sh Computos.sh 
+    fi
+  fi
 done
